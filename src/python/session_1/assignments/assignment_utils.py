e0442dbb9237d46d494135750d3903fc75cd2535
Mahmoud Taha <mahmouddttahaa@gmail.com>
2025-12-18 19:57:50 +0200

    Completed python session 1

---
+++ src/python/session_1/assignments/assignment_utils.py
@@ -0,0 +1,51 @@
+def extract_core_part(s: str) -> str:
+	"""
+	extracts the core part of string (i.e. all alphabetic characters and spaces).
+	parameters:
+		s (str): the raw string from which the core part will be extracted
+	returns:
+		core part of a string which is also a string
+	"""
+	return "".join(char for char in s if char.isalpha() or char == " ")
+
+def replace_letters(s: str, mapping: dict) -> str:
+	"""
+	replaces letters in a given string with their respective mappings from the mapping dict.
+	parameters:
+		s (str): input string to be processed
+		mapping (dict): a dictionary containing the letters to be replaced as its keys and the letter replacements as its values
+	returns:
+		updated string with replaced letters as prescribed in the mapping dict
+	"""
+	str_lst = list(s)   
+	replaced_lst = []
+	for char in str_lst:
+		if char not in mapping.keys():
+			replaced_lst.append(char)
+		else:
+			replaced_lst.append(mapping[char])
+
+	replaced_str = "".join(replaced_lst)
+
+	return replaced_str
+
+def reverse_str(s: str) -> str:
+	"""
+	reverses a given string (e.g. "Hello" -> "olleH")
+		parameters:
+		s (str): input string to be reversed
+	returns:
+		reversed version of the inupt string
+	"""
+	return s[::-1]
+
+
+def last_to_first(s: str) -> str:
+	"""
+	moves the last char to the beginning of the string (e.g. "Hello" -> "oHell")
+		parameters:
+		s (str): input string to be processed
+	returns:
+		s with its last char moved to the front
+	"""
+	return s[-1] + s[:-1]
